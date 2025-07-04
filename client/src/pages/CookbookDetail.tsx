c4b25e93eb722d6e99f8736fa473bec7e5d86d9d
Michael Cohen <macball2000@yahoo.com>
2025-07-03 23:23:47 -0400

    CookbookDetail: Fixed Button Text

--- client/src/pages/CookbookDetail.tsx
+++ client/src/pages/CookbookDetail.tsx
@@ -104,14 +104,14 @@ const CookbookDetail = () => {
                     d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
                   />
                 </svg>
-                Edit Recipe
+                Edit Cookbook
               </Link>
               <button
                 onClick={handleDelete}
                 className='inline-flex items-center px-4 py-2 rounded bg-gray-100 text-gray-700 hover:bg-red-500 hover:text-white transition-colors cursor-pointer'
               >
                 <TrashIcon className='mr-2' />
-                Delete Recipe
+                Delete Cookbook
               </button>
             </>
           )}
