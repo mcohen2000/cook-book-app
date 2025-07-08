43e30e36a18c037e074c864da7da866d9db5af65
Michael Cohen <macball2000@yahoo.com>
2025-07-07 23:44:22 -0400

    RecipeCard: UI Fixes

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router';
+import ClockIcon from './icons/ClockIcon';
+import ServingsIcon from './icons/ServingsIcon';
 
 interface RecipeCardProps {
   id: string;
@@ -23,35 +25,11 @@ const RecipeCard = ({
           <p className='text-gray-600 mb-4 line-clamp-2'>{description}</p>
           <div className='flex items-center justify-between text-sm text-gray-500'>
             <div className='flex items-center'>
-              <svg
-                className='w-4 h-4 mr-1'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              <ClockIcon className='w-4 h-4 mr-1' />
               {cookingTime} min
             </div>
             <div className='flex items-center'>
-              <svg
-                className='w-4 h-4 mr-1'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
-                />
-              </svg>
+              <ServingsIcon className='w-4 h-4 mr-1' />
               {servings} servings
             </div>
           </div>
