ef065be7d352514328dd5636af1be30e0b8537af
Michael Cohen <macball2000@yahoo.com>
2025-07-07 23:36:10 -0400

    CookbookCard: UI Fixes

--- client/src/components/CookbookCard.tsx
+++ client/src/components/CookbookCard.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router';
 import type { Book } from '../types/book';
+import BookIcon from './icons/BookIcon';
+import UserIcon from './icons/UserIcon';
 
 interface CookbookCardProps {
   cookbook: Book;
@@ -26,35 +28,12 @@ const CookbookCard = ({ cookbook }: CookbookCardProps) => {
           )}
           <div className='flex items-center justify-between text-sm text-gray-500 mb-4'>
             <div className='flex items-center'>
-              <svg
-                className='w-4 h-4 mr-1'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
-                />
-              </svg>
-              {cookbook.recipes.length} recipes
+              <BookIcon className='w-4 h-4 mr-1' />
+              {cookbook.recipes.length}{' '}
+              {cookbook.recipes.length === 1 ? 'recipe' : 'recipes'}
             </div>
             <div className='flex items-center'>
-              <svg
-                className='w-4 h-4 mr-1'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
-                />
-              </svg>
+              <UserIcon className='w-4 h-4 mr-1' />
               {authorName}
             </div>
           </div>
