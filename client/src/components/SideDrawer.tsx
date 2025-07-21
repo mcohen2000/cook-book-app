21c0feab14656668bef8e847ea6cfd6b35586c6c
Michael Cohen <macball2000@yahoo.com>
2025-07-20 23:37:43 -0400

    SideDrawer: Updated Styles

--- client/src/components/SideDrawer.tsx
+++ client/src/components/SideDrawer.tsx
@@ -11,7 +11,7 @@ const SideDrawer: React.FC<SideDrawerProps> = ({ open, onClose }) => {
     <>
       {/* Overlay */}
       <div
-        className={`fixed inset-0 bg-black bg-opacity-30 transition-opacity duration-300 z-40 ${
+        className={`fixed inset-0 bg-black/50 transition-opacity duration-300 z-40 ${
           open
             ? 'opacity-100 pointer-events-auto'
             : 'opacity-0 pointer-events-none'
